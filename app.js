03f5c8ccb71e9a9026fb4e35b77f3f1e73797348
gavin-white <gavin.white0001@gmail.com>
2023-04-10 17:59:33 -0400

    fix app.js

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
 import express from 'express';
 import cors from 'cors';
-import HelloController
-  from "./controllers/hello-controller.js";
-import UserController
-  from "./controllers/users/users-controller.js";
-import TuitsController
-  from "./controllers/tuits/tuits-controller.js";
+import BookclubsController
+  from "./controllers/bookclubs/bookclubs-controller.js";
+import UsersController
+  from "./controllers/bookclubs/bookclubs-controller.js";
 import mongoose from "mongoose";
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
-    || 'mongodb://127.0.0.1:27017/tuiter';
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
 await mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(cors());
 app.use(express.json());
-TuitsController(app);
-HelloController(app);
-UserController(app);
+BookclubsController(app);
+UsersController(app);
 app.listen(process.env.PORT || 4000);
