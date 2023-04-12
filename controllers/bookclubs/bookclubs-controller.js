05aed5adac2f664f2305697ef03a2d5dc8884234
Julia Kerr <juliakerr26@gmail.com>
2023-04-12 13:03:24 -0400

    renaming

--- controllers/bookclubs/bookclubs-controller.js
+++ controllers/bookclubs/bookclubs-controller.js
@@ -1,38 +1,33 @@
-import * as bookClubsDao from "./bookclubs-dao.js";
+import * as bookClubsDao from './bookclubs-dao.js';
 
-const createBookclub = async (req, res) => {
-  const newBookclub = req.body;
-  newBookclub.bookList = [];
-  const insertedBookclub = await bookClubsDao
-  .createBookclub(newBookclub);
-  res.json(insertedBookclub);
-}
+const createBookClub = async (req, res) => {
+  const newBookClub = req.body;
+  newBookClub.bookList = [];
+  const insertedBookClub = await bookClubsDao.createBookClub(newBookClub);
+  res.json(insertedBookClub);
+};
 
-const findBookclub = async (req, res) => {
-  const bookClub = await bookClubsDao.findBookclub()
+const findBookClubs = async (req, res) => {
+  const bookClub = await bookClubsDao.findBookClubs();
   res.json(bookClub);
-}
+};
 
-const updateBookclub = async (req, res) => {
-  const bookclubIdToUpdate = req.params.bid;
+const updateBookClub = async (req, res) => {
+  const bookClubIdToUpdate = req.params.bid;
   const updates = req.body;
-  const status = await bookClubsDao
-  .updateBookclub(bookclubIdToUpdate,
-      updates);
+  const status = await bookClubsDao.updateBookClub(bookClubIdToUpdate, updates);
   res.json(status);
-}
+};
 
-const deleteBookclub = async (req, res) => {
-  const bookclubIdToDelete = req.params.bid;
-  const status = await bookClubsDao
-  .deleteBookclub(bookclubIdToDelete);
+const deleteBookClub = async (req, res) => {
+  const bookClubIdToDelete = req.params.bid;
+  const status = await bookClubsDao.deleteBookClub(bookClubIdToDelete);
   res.json(status);
-}
+};
 
-export default (app) => {
-  app.post('/api/bookclubs', createBookclub);
-  app.get('/api/bookclubs/:bid', findBookclub);
-  app.put('/api/bookclubs/:bid', updateBookclub);
-  app.delete('/api/bookclubs/:bid', deleteBookclub);
-}
-
+export default app => {
+  app.post('/api/bookclubs', createBookClub);
+  app.get('/api/bookclubs/:bid', findBookClubs);
+  app.put('/api/bookclubs/:bid', updateBookClub);
+  app.delete('/api/bookclubs/:bid', deleteBookClub);
+};
