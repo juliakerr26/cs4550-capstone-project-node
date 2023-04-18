a58f9da945c7823154cb22ebe6ccb8f4be1f47a3
Julia Kerr <juliakerr26@gmail.com>
2023-04-18 16:21:00 -0400

    rename bookclub users to members

--- controllers/bookclubs/bookclubs-schema.js
+++ controllers/bookclubs/bookclubs-schema.js
@@ -2,6 +2,6 @@ import mongoose from 'mongoose';
 const schema = mongoose.Schema({
   admin: String,
   bookList: [],
-  users: [],
+  members: [],
 }, {collection: 'bookclubs'});
 export default schema;
